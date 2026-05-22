50a7f8919f464a2777c65028e1b8c2a32c57968b
austinritchie24 <austin.ritchie24@utexas.edu>
2026-05-22 13:17:00 -0500

    Update spotify-token.js

--- netlify/functions/spotify-token.js
+++ netlify/functions/spotify-token.js
@@ -1,1 +1,31 @@
+exports.handler = async function(event, context) {
+  const clientId     = '8b7dad9d0d334b6ca4f081fab3d98d9e';
+  const clientSecret = '276548f4d41d4ca8acae052548e3fee8';
 
+  try {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+      },
+      body: 'grant_type=client_credentials'
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ access_token: data.access_token, expires_in: data.expires_in })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
